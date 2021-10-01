f100b3bd70a2a4ad29dc0654fabba2a2e259a245
elbertmartins <elbert.martins@conforce.com.br>
2021-10-01 16:43:57 -0300

    Alterando logo

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,42 +1,39 @@
-import'./Menu.css'
-import React from 'react'
+import "./Menu.css";
+import React from "react";
 
-    export default function Main (){
-        return(
-          
-            <header className="cabeçalho">
+export default function Main() {
+  return (
+    <header className="cabeçalho">
       <div className="logo">
         <a href="#inicio">
-          <img src="conforce.jpg" alt="Logo" />
+          <img src="pexels-photo-3750272.jpeg" alt="Logo" />
         </a>
-        </div>
-        <button className="menu-toggle">
-          <i className="fa fa-lg fa-bars"></i>
-        </button>
-          <nav className="menu">
-            <ul>
-            <li>
-              <a href="#inicio">Inicio</a>
-            </li>
-            <li>
-              <a href="#Cursos">Cursos</a>
-            </li>
-            <li>
-              <a href="#Sobre">Sobre</a>
-            </li>
-            <li>
-              <a href="#Contato">Contato</a>
-            </li>
-          </ul>
-        </nav>
-        <aside className="autenticacao">
-          <a href="#login">Login</a>
-          <a href="#registrar" class="botao destaque">Registrar</a>
-        </aside>
-
+      </div>
+      <button className="menu-toggle">
+        <i className="fa fa-lg fa-bars"></i>
+      </button>
+      <nav className="menu">
+        <ul>
+          <li>
+            <a href="#inicio">Inicio</a>
+          </li>
+          <li>
+            <a href="#Cursos">Cursos</a>
+          </li>
+          <li>
+            <a href="#Sobre">Sobre</a>
+          </li>
+          <li>
+            <a href="#Contato">Contato</a>
+          </li>
+        </ul>
+      </nav>
+      <aside className="autenticacao">
+        <a href="#login">Login</a>
+        <a href="#registrar" class="botao destaque">
+          Registrar
+        </a>
+      </aside>
     </header>
-        
-        )
-    }
-        
-    
+  );
+}
